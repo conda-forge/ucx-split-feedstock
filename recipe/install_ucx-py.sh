96145077d942be89a6ff0761f5fc948b13aaacb9
John Kirkham <jakirkham@gmail.com>
2019-10-16 17:29:10 -0400

    Drop `--with-cuda` flag from `build_ext`
    As the `--with-cuda` flag has been dropped from ucx-py (since it is no
    longer needed), drop it from ucx-py's `install_ucx-py.sh` script.

--- recipe/install_ucx-py.sh
+++ recipe/install_ucx-py.sh
@@ -2,10 +2,5 @@
 
 set -xeuo pipefail
 
-CUDA_CONFIG_ARG=""
-if [ ${cuda_compiler_version} != "None" ]; then
-    CUDA_CONFIG_ARG="--with-cuda"
-fi
-
 cd "${SRC_DIR}/ucx-py"
-$PYTHON setup.py build_ext ${CUDA_CONFIG_ARG} install
+$PYTHON setup.py build_ext install
